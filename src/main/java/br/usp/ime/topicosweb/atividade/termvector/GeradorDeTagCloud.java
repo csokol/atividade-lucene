1cabbb6887d422c972d9645566f583f4b1591a0a
Francisco Sokol <chico.sokol@gmail.com>
2012-08-12 12:59:56 -0300

    melhorando geradow

--- src/main/java/br/usp/ime/topicosweb/atividade/termvector/GeradorDeTagCloud.java
+++ src/main/java/br/usp/ime/topicosweb/atividade/termvector/GeradorDeTagCloud.java
@@ -3,22 +3,22 @@ package br.usp.ime.topicosweb.atividade.termvector;
 import java.util.List;
 
 public class GeradorDeTagCloud {
-    private static final String HEADER = "<html><head><title>Tag Cloud</title></head><body>";
-    private static final String FOOTER = "</body></html>";
-    
+    private static final String HEADER = "<html><head><title>Tag Cloud</title></head><body><div style='margin:auto; width=900px; text-align:center;'>";
+    private static final String FOOTER = "</div></body></html>";
+
     public String gera(TermVector termVector) {
         StringBuffer html = new StringBuffer(HEADER);
         html.append("<h1>Tag cloud!</h1>");
         List<TagMagnitude> magnitudes = termVector.getMagnitudes();
         for (TagMagnitude tagMagnitude : magnitudes) {
             int magnitude = (int) tagMagnitude.getMagnitude();
-            html.append("<a href='#' style=' margin-left:10px; font-size:");
-            html.append(magnitude*5 + "px;'>");
+            html.append("<a href='#' style='float:left; margin-left:10px; font-size:");
+            html.append(magnitude * 3 + "px;'>");
             html.append(tagMagnitude.getTag());
             html.append("</a>");
         }
         html.append(FOOTER);
         return html.toString();
     }
-    
+
 }
