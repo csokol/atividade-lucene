ff480b154613387e6f66058463e82907e1506548
Francisco Sokol <chico.sokol@gmail.com>
2012-08-12 01:31:50 -0300

    fim da atividade... muito facil :(

--- src/main/java/br/usp/ime/topicosweb/atividade/Main.java
+++ src/main/java/br/usp/ime/topicosweb/atividade/Main.java
@@ -1,24 +1,35 @@
 package br.usp.ime.topicosweb.atividade;
 
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.StringReader;
+import java.io.Reader;
 
 import org.apache.lucene.analysis.Token;
 import org.apache.lucene.analysis.TokenStream;
 
 import br.usp.ime.topicosweb.atividade.analyzer.MeuAnalyzer;
+import br.usp.ime.topicosweb.atividade.termvector.GeradorDeTagCloud;
+import br.usp.ime.topicosweb.atividade.termvector.TermVector;
 
 public class Main {
+    
     public static void main(String[] args) throws IOException {
         MeuAnalyzer meuAnalyzer = new MeuAnalyzer();
-        String text = "Programação orientada a objetos é uma péssima ideia, " +
-                "que só poderia ter nascido na Califórnia.";
-        StringReader reader = new StringReader(text);
+        
+        Reader reader = new FileReader("src/main/resources/artigo.txt");
+        
         TokenStream tokenStream = meuAnalyzer.tokenStream(null, reader);
         Token token = tokenStream.next();
+        TermVector termVector = new TermVector();
         while (token != null) {
-            System.out.println(token.termText());
+            termVector.processaTermo(token.termText());
             token = tokenStream.next();
         }
+        GeradorDeTagCloud gerador = new GeradorDeTagCloud();
+        String html = gerador.gera(termVector);
+        FileWriter writer = new FileWriter("cloud.html");
+        writer.write(html);
     }
+    
 }
